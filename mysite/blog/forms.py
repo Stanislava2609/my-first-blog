436397a0a9d1f09488e7882db169892abdbc6c9f
Stanislava <you@example.com>
2019-01-30 22:53:00 +0300

    Красивые поля

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.forms import Textarea, TextInput
 
 from .models import Post
 
-class PostForm(forms.ModelForm):
 
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'image')
+        labels = {'title': 'Заголовок', 'text':'Текст', 'image':'Изображение'}
+        widgets = {
+            'text': Textarea(attrs={'class':"form-control", 'rows':'3', 'style':'resize:none'}),
+            'title': TextInput(attrs={'class':"form-control", 'placeholder':"Заголовок"})
+        }
