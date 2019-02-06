b30084cefaf416ddbe02a6587b3bbd92e814732f
Stanislava <you@example.com>
2019-02-06 18:56:31 +0300

    Добавила логин

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('post/<int:pk>', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('artem/', views.artem, name='artem')
+    path('artem/', views.artem, name='artem'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
